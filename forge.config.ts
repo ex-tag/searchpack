72c4b1757bddf83fc3762a47734ce87b460fc4d2
Tim Montague <1149793+ex-tag@users.noreply.github.com>
2024-03-23 05:06:46 -0700

    Update: icons

--- forge.config.ts
+++ forge.config.ts
@@ -10,7 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    icon: 'icon',
+    icon: 'icons',
   },
   rebuildConfig: {},
   makers: [
